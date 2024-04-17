1bacf93f48fdf97d042ef8ad7fb7d8aa805563e7
Thiago Academia CRM <119423088+ThiagooMagu@users.noreply.github.com>
2024-04-16 22:26:19 -0300

    Atualizar a paleta de cores para incluir tons de branco e vermelho para destaque

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @type {import('tailwindcss').Config}
+ */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -6,6 +8,15 @@ module.exports = {
       colors: {
         slate: {
           800: "#141E33",
+        },
+        // Adicionando a nova paleta de cores
+        white: {
+          DEFAULT: "#FFFFFF",
+          dim: "#F7F7F7",
+        },
+        red: {
+          DEFAULT: "#FF0000", // Vermelho para destaque
+          dark: "#B20000",
         },
       },
       screens: {
